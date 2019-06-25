efc6f2f59425936e9edcf18edaa60111f4feba20
小白-白 <xb@fczbl.vip>
2019-06-25 13:19:25 +0800

    [Update] V1

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center">
+<img src="https://img.fczbl.vip/images/2019/06/25/lmgtfy.png" alt="让我帮你谷歌一下">
+</p>
+
+<h3 align="center"> 「Let Me Google That For You」</h3>
+
+<p align="center">
+<a href="https://www.fczbl.vip"><img alt="Author" src="https://img.shields.io/badge/Author-Moedog-blue.svg?style=flat-square"></a>
+<img alt="License" src="https://img.shields.io/github/license/xb2016/lmgtfy.svg?style=flat-square"/>
+</p>
+
+## 基本介绍
+
+创意来自于「让我帮你百度一下」，这玩意出来好久了，同时，「让我帮你谷歌一下」也有，不过有各种不方便，要么就是年代久远，用的旧样式，要么就是境内访问有点难度。所以抽空改了一个出来。
+
+基于这两个项目：[https://github.com/bangbang93/lmbtfy.cn](https://github.com/bangbang93/lmbtfy.cn) 以及 [https://github.com/mengkunsoft/lmbtfy](https://github.com/mengkunsoft/lmbtfy)
+
+演示站点：[https://www.fczbl.vip/google/](https://www.fczbl.vip/google/)
+
+## 食用注意
+
+首先，环境需要 PHP，用于生成短链接用的；其次，要提到这个程序的一个优点同时也是缺点，它可以判断使用者的网络能不能访问谷歌，如果不能，则调用反代站点访问。
+
+但是这个反代站需要程序搭建者自己准备了，本程序自带的只能由我站调用。同时这个判断方法有点残暴，会在控制台报错，如果有更优雅的方法，欢迎 PR 。
+
+编辑 lmgtfy.js ，把 www.loli.beer 换成你自己的谷歌反代链接，实在没有就换成 www.google.com 吧。
+
+## 捐赠
+
+- [https://pay.fczbl.vip](https://pay.fczbl.vip)
+
+## Author
+
+**lmgtfy** © [Moedog](https://github.com/xb2016), Released under the [MIT](./LICENSE) License.<br>
+
+> Blog [fczbl.vip](https://www.fczbl.vip) · GitHub [@xb2016](https://github.com/xb2016) · Twitter [@fczbl](https://twitter.com/fczbl)
---
+++ js/lmgtfy.js
@@ -0,0 +1,117 @@
+/**
+ * Created by bangbang on 14/10/10.
+ * Revised by mengkun on 17/03/16.
+ * Updated by Moedog on 19/06/17.
+ */
+ 
+function urlEncode(String){
+    return encodeURIComponent(String).replace(/'/g,"%27").replace(/"/g,"%22");    
+}
+ 
+$(document).ready(function(){
+    var copy = new Clipboard('#copy');
+    $('#search').on('click',function(){
+        var link = window.location.origin + window.location.pathname + '?' + urlEncode($('#kw').val());
+        $.ajax({ 
+            url: 'get.php?longUrl='+link,  
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            success: function (data) {
+                if (data){
+                    if(!(typeof data.urls === undefined || typeof data.urls == "undefined"))   //防止短网址失败
+                    {
+                        link = data.urls[0].url_short;
+                    }
+                }
+                $('#go').attr("href",link);
+                $('#link').show();
+                $('#instructions').text('复制下面的地址,然后发给伸手党吧！');
+                $('#lmgtfyLink').val(link).focus().select();
+            }
+        })
+    });
+    $('#search2').on('click',function(){
+        if($(".search-text").attr("data-site")=="google"){window.location = 'https://www.google.com.hk/search?q=' + urlEncode($('#kw').val());}else{window.location = 'https://www.loli.cab/search?q=' + urlEncode($('#kw').val());}
+    });
+    var $container = $('.container');
+    $container.on('click','#go',function(){
+        var link = $('#lmgtfyLink').val();
+        if (!!link){
+            window.open(link);
+            //window.location = link;
+        }
+    });
+    var $kw = $('#kw');
+    $kw.on('keydown', function (e) {
+        if (e.keyCode == 13){
+            $('#search').trigger('click');
+        }
+    });
+    if (!!window.location.search){
+        var kw = decodeURIComponent(window.location.search.substr(1));
+        var $instructions = $('#instructions');
+        var $arrow = $('#arrow');
+        setTimeout(function (){
+            $instructions.text('1、找到输入框并选中');
+            $arrow.show().animate({
+                left: $kw.offset().left + 10 + 'px',
+                top: ($kw.offset().top + $kw.height()/2) + 'px'
+            }, 2000, function (){
+                $instructions.text('2、输入你的问题');
+                $arrow.hide();
+                var $kw = $('#kw');
+                $kw.focus();
+                var i = 0;
+                var interval = setInterval(function (){
+                    $kw.val(kw.substr(0,i));
+                    i++;
+                    if (i > kw.length){
+                        clearInterval(interval);
+                        $instructions.text('3、按下“Google 搜索”按钮');
+                        $arrow.show();
+                        var $search = $('#search');
+                        $arrow.animate({
+                            left: $search.offset().left + $search.width()/2 + 'px',
+                            top: $search.offset().top + $search.height()/2 + 'px'
+                        }, 1000, function () {
+                            $instructions.html('<strong>这对你而言就是那么困难么？</strong>');
+                            setTimeout(function (){
+                                if($(".search-text").attr("data-site")=="google"){window.location = 'https://www.google.com.hk/search?q=' + encodeURIComponent(kw);}else{window.location = 'https://www.loli.cab/search?q=' + encodeURIComponent(kw);}
+                            }, 2000);
+                        })
+                    }
+                }, 200);
+            });
+        }, 1000);
+    }
+});
+
+function showAbout(){
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var popupHeight = $("#msgbox").height();       
+    var popupWidth = $("#msgbox").width(); 
+    $("#mask").width(windowWidth).height(windowHeight).click(function(){hideAbout();}).fadeIn(200); 
+    $("#msgbox").css({"position": "absolute","left":windowWidth/2-popupWidth/2,"top":windowHeight/2-popupHeight/2}).fadeIn(200); 
+}
+
+function hideAbout(){
+    $("#mask").fadeOut(200);
+    $("#msgbox").fadeOut(200); 
+}
+
+function gtest(){
+    $.ajax({
+        type:"get",
+        cache:false,
+        url:"https://www.google.com.hk/",
+        dataType:"jsonp",
+        processData:false,
+        timeout:5000,
+        complete:function(data){
+            if(data.status==200){$(".search-text").attr("data-site","google");}else{$(".search-text").attr("data-site","google2");}
+        }
+    });
+}
+window.onload = function(){gtest();window.setInterval("gtest()",10000);}
